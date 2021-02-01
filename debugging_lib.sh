7a5eccf153adc0721703f816773fde5a1ceea596
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-01-31 22:44:59 -0800

    ext: add command to debug dlopen
    Change-Id: Ie6eac0e4ae62f25d8659a262c3710acc462183a1
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

---
+++ debugging_lib.sh
@@ -0,0 +1,1 @@
+LD_DEBUG=libs env LD_LIBRARY_PATH="/scr/hn/gem5-sst/build/X86/" sst --add-lib-path /scr/hn/gem5-sst/ext/sst2/ ./ext/sst2/tests/test_x86.py
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-env LD_LIBRARY_PATH="/scr/hn/gem5-sst/build/X86/" sst --add-lib-path /scr/hn/gem5-sst/ext/sst2/ ./ext/sst2/tests/test_x86.py
+env LD_LIBRARY_PATH="/scr/hn/gem5-sst/lib/" sst --add-lib-path /scr/hn/gem5-sst/ext/sst2/ ./ext/sst2/tests/test_x86.py
 
